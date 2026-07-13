e0babe921ea27eb67e2bc42feab890fa268e69ed
mintenthusiast <adenchen0219@gmail.com>
2026-07-13 10:33:59 +0800

    update 7-13-10:33

--- CNN/cnn-hand.py
+++ CNN/cnn-hand.py
@@ -4,17 +4,25 @@ import torchvision
 class CNN:
     def __init__(self, stride, learning_rate, epochs, hidden_size):
         self.stride = stride
-        self.learning_rate = learning_rate
+        self.lr = learning_rate
         self.epochs = epochs
 
         self.kernel = np.array([[1, 1, 1], 
                                [1, -8, 1],
                                [1, 1, 1]])
         
+        self.b_kernel = np.zeros_like(self.kernel)
+        
         self.kernel_x, self.kernel_y = 3, 3
 
-        self.h1 = self.weight_init(, hidden_size)
-        self.out = self.weight_init()
+
+        self.w_h1 = self.weight_init(1024, hidden_size)
+        self.w_out = self.weight_init(hidden_size, 10)
+
+        self.b_h1 = np.zeros_like(self.w_h1)
+        self.b_out = np.zeros_like(self.w_out)
+
+        self.a_h1, self.a_out, self.a_conv = 0, 0, 0
         
         # need to randomise kernel weights maybe
 
@@ -45,6 +53,7 @@ class CNN:
                         section = image_padded[x:x + self.kernel_x, y:y+self.kernel_y]
                         f_map[x, y] = np.sum(self.kernel * section)
 
+        self.a_conv = f_map
         return f_map
 
     def pool(self, f_map):
@@ -66,14 +75,57 @@ class CNN:
     def relu(self, x):
         return np.maximum(0, x).astype(float)
     
-    def d_reLu(self, x):
+    def d_relu(self, x):
         return np.where(x > 0, 1, 0)
+    
+    def softmax(self, x):
+        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+    
+    def cross_entropy_loss(self, p, y):
+        loss = -np.sum(y * np.log(p + 1e-15), axis=1)
+        return loss
+    
 
     def weight_init(self, in_size, out_size):
         std = np.sqrt(2.0 / in_size)
         weights = np.random.randn(in_size, out_size) * std
         return weights
 
+    def forward(self, x):
+        z_h1 = np.dot(x, self.w_h1) + self.b_h1
+        self.a_h1 = self.relu(z_h1)
+        z_out = np.dot(self.a_h1, self.w_out) + self.b_out
+        self.a_out = self.softmax(z_out)
 
+    def backward(self, x, y):
+        m = x.shape[0]
+        self.forward(x)
 
+        d_out = (self.a_out - y) / m
 
+        err_h1 = np.dot(d_out, self.w_out.T)
+        d_h1 = err_h1 * self.d_relu(self.a_h1)
+
+        err_conv = np.dot(d_h1, self.kernel.T)
+        d_conv = err_conv * self.d_relu(self.a_conv)
+        
+        self.w_out += np.dot(self.a_h1.T, self.d_out) * self.lr
+        self.w_h1 += np.dot(x.T, d_h1) * self.lr
+        self.kernel += np.dot(self.a_out.T, self.d_conv) * self.lr
+
+        self.b_out += np.sum(d_out, axis=0, keepdims=True) * self.lr
+        self.b_h1 += np.sum(d_h1, axis=0, keepdims=True) * self.lr
+        self.b_kernel += np.sum(d_conv, axis=0, keepdims=True) * self.lr
+
+    def fit(self, x, y):
+        for epoch in range(self.epochs):
+            f_map = self.pool(self.relu(self.convolute(x)))
+            self.backward(f_map, y)
+            if epoch % 10 == 0:
+                loss = self.cross_entropy_loss(self.a_out, y)
+                print(f"Epoch: {epoch}, Loss: {np.mean(loss)}")
+
+    def predict(self, x):
+        self.forward(x)
+        return self.a_out
